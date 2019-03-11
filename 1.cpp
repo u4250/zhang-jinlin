6db0c2d05fa14922a59d7fc4fefe74ae8fbb0ade
youname <1055991127@qq.com>
2019-03-11 20:26:41 +0800

    作业的一个小题

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,sum,p,o,u;
+    cout <<"请输入两个整数:";
+    cin >>a>>b;
+    sum = a+b;
+    p = a-b;
+    o = a*b;
+    u = a/b;
+    cout<<"a+b="<<sum<<"\n";
+    cout<<"a-b="<<p<<"\n";
+    cout<<"a*b="<<o<<"\n";
+    cout<<"a/b="<<u<<"\n";
+
+    return 0;
+}
+
