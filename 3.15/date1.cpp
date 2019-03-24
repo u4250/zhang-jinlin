6394a7d7c91770b1b330ada5ea6b807805254aaa
youname <1055991127@qq.com>
2019-03-24 15:04:30 +0800

    3.15三个文件分开

---
+++ 3.15/date1.cpp
@@ -0,0 +1,15 @@
+class Date 
+{
+public:
+   void setMonth( int ); 
+   int getMonth(); 
+   void setDay( int ); 
+   int getDay(); 
+   void setYear( int );
+   int getYear(); 
+   void displayDate(); 
+private:
+   int Month; 
+   int Day; 
+   int Year; 
+};
