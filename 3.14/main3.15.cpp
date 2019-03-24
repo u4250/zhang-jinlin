06977ab1362243a5d9d3b5c4e856dee9a3068c1c
youname <1055991127@qq.com>
2019-03-24 14:57:46 +0800

    3.14

---
+++ 3.14/main3.15.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "date.h"
+
+main()
+{
+	int a,b,c;
+	Date Mydate;
+	Mydate.setmonth(a);
+	//cout<<Mydate.getmonth();
+	Mydate.setday(a);
+	//cout<<Mydate.getday();
+	Mydate.setyear(a);
+//	cout<<Mydate.getyear();
+	Mydate.displayDate();
+	
+}
+
